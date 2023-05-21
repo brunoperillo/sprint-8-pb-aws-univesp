ccf5ac0865e60270f9ed191f3202fe9b0a15d284
M Vinicios Nativo <onativo@outlook.com>
2023-05-20 22:25:46 -0300

    fix: update lambda function to /v1/vision route

--- visao-computacional/handler.py
+++ visao-computacional/handler.py
@@ -38,90 +38,69 @@ class CustomEncoder(json.JSONEncoder):
     
 def v1Label(event, context):
     # Extract bucket name and image name from the POST request body
-    bucket_name = event['bucket']
-    image_name = event['imageName']
-    
-    #S3 connection
-    s3_client = boto3.client('s3')
-    
-    # Create an Amazon Rekognition client
-    rekognition = boto3.client("rekognition")
-
-    # Call Amazon Rekognition to detect labels in the image
-    response = rekognition.detect_labels(
-        Image={'S3Object': {'Bucket': bucket_name, 'Name': image_name}},
-        MaxLabels=5  # Change this value for more or fewer labels
-    )
-
-    # Extract the detected labels from the response
-    labels = [{'confidence': label['Confidence'], 'label': label['Name']} for label in response["Labels"]]
-    
-    
-    # Get the metadata of the image object
     try:
+        body = json.loads(event['body'])
+        bucket_name = body['bucket']
+        image_name = body['imageName']
+    except KeyError as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': f'Missing required field: {e}'})
+        }
+    except json.JSONDecodeError:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Invalid JSON payload'})
+        }
+    
+    try:
+        # S3 connection
+        s3_client = boto3.client('s3')
+        # Create an Amazon Rekognition client
+        rekognition = boto3.client("rekognition")
+        
+        # Call Amazon Rekognition to detect labels in the image
+        response = rekognition.detect_labels(
+            Image={'S3Object': {'Bucket': bucket_name, 'Name': image_name}},
+            MaxLabels=5  # Change this value for more or fewer labels
+        )
+
+        # Extract the detected labels from the response
+        labels = [{'confidence': label['Confidence'], 'label': label['Name']} for label in response["Labels"]]
+        
+        # Get the metadata of the image object
         response = s3_client.head_object(Bucket=bucket_name, Key=image_name)
         image_url = f"https://{bucket_name}.s3.amazonaws.com/{image_name}"
         creation_time = response['LastModified']
-    except Exception as e:
+        
+        # Create a dictionary representing the JSON body
+        response_body = {
+            "url_to_image": image_url,
+            "created_image": creation_time,
+            "labels": labels
+        }
+
+        # Convert the dictionary to a JSON string using the custom encoder
+        response_json = json.dumps(response_body, cls=CustomEncoder)
+        
+        # Print logs to CloudWatch((?))
+        print(response_json)
+
+        # Return the JSON string as the response body of the Lambda function
+        return {
+            'statusCode': 200,
+            'body': response_json
+        }
+        
+    except boto3.exceptions.S3Exception as e:
         return {
             'statusCode': 400,
             'body': json.dumps({'error': str(e)})
         }
-        
-    # Convert the datetime object to a string in ISO 8601 format
-    creation_time_str = creation_time.isoformat()
-
-    # Create a dictionary representing the JSON body
-    response_body = {
-        "url_to_image": image_url,
-        "created_image": creation_time,
-        "labels": labels
-    }
     
-    print(response_body)
-
-    # Convert the dictionary to a JSON string using the custom encoder
-    response_json = json.dumps(response_body, cls=CustomEncoder)
-
-    print(response_json)
-    
-    # Return the JSON string as the response body of the Lambda function
-    return {
-        'statusCode': 200,
-        'body': response_json
-    }
-'''
-def v1Label(event, context):
-    # Extract bucket name and image name from the POST request body
-    bucket_name = event['bucket']
-    image_name = event['imageName']
-
-    # Create an Amazon Rekognition client
-    rekognition = boto3.client('rekognition')
-
-    # Call Amazon Rekognition to detect labels in the image
-    response = rekognition.detect_labels(
-        Image={'S3Object': {'Bucket': bucket_name, 'Name': image_name}},
-        MaxLabels=10  # Change this value if you want more or fewer labels
-    )
-
-    labels = []
-    # Extract the detected labels from the response
-    print('Detected labels for image: ' + image_name)
-    print()
-    for label in response['Labels']:
-        print ("Confidence: " + str(label['Confidence']))
-        print ("Label: " + label['Name'])
-        print('---------------|')
-    # Return the labels as the response of the Lambda function
-    
-    
-    return {"statusCode": 200, "body": json.dumps(body)}'''
-
-
-def v2Emotion():
+def v2Emotion(event, context):
     body = {
-        "message": "Emotion API gateway is working"
+        "message": "funcionou a função /v2/vision"
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
