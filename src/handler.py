080d8b3a9d3d7bd4e64650a88e32ddff36005b0f
Carlos Roberto S Camilo <carlos.rs.camilo@unesp.br>
2023-05-26 18:45:58 -0300

    :sparkles: feat: adicionado codigo para /v1/vision - reconhecimento de rótulos

---
+++ src/handler.py
@@ -0,0 +1,79 @@
+import json
+import boto3
+
+def health(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+def v1_description(event, context):
+    body = {
+        "message": "VISION api version 1."
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+def v2_description(event, context):
+    body = {
+        "message": "VISION api version 2."
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+def v1_vision(event, context):
+    
+    request_body = json.loads(event['body'])
+    
+    bucket = request_body['bucket']
+    imageName = request_body['imageName']
+    
+    url_to_image = f"https://{bucket}/{imageName}"
+    
+    # Conectar-se ao AWS Rekognition
+    rekognition = boto3.client('rekognition')
+    
+    # Executar a detecção de rótulos na imagem
+    response = rekognition.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': imageName
+            }
+        }
+    )
+    
+    # Extrair os rótulos e confiança associada
+    labels = []
+    for label in response['Labels']:
+        labels.append({
+            'Name': label['Name'],
+            'Confidence': label['Confidence']
+        })
+    
+    result = {
+        'url_to_image': url_to_image,
+        'created_image': '02-02-2023 17:00:00',
+        'labels': labels
+    }
+    
+    print(result)
+    
+    # Retornar uma resposta HTTP válida
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(result),
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
+        
+    return response
