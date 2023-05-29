fea388a3d234b7f4f6ac3ca013cd281cf044ee0b
Carlos Roberto S Camilo <carlos.rs.camilo@unesp.br>
2023-05-28 21:42:24 -0300

    :package: refactor: incluido tratamento requisicoes com erro - img nao existe

--- src/vision1.py
+++ src/vision1.py
@@ -1,53 +1,53 @@
 import boto3
+import botocore.exceptions
 import json
 from .utils import get_image_creation_date, create_http_response
 
 def v1_vision(event, context):
 
-    request_body = event.get('body')
+    request_body = json.loads(event['body'])
+    bucket = request_body['bucket']
+    imageName = request_body['imageName']
+    url_to_image = f"https://{bucket}/{imageName}"
+    
+    # Verificar se a imagem existe no bucket
+    s3 = boto3.client('s3')
+    try:
+        s3.head_object(Bucket=bucket, Key=imageName)
 
-    if request_body:
-        request_body = json.loads(request_body)
-        bucket = request_body.get('bucket')
-        imageName = request_body.get('imageName')
-
-        if bucket and imageName:
-            url_to_image = f"https://{bucket}/{imageName}"
-
-            # Conecta-se ao AWS Rekognition e seleciona o serviço de reconhecimento de rótulos
-            rekognition = boto3.client('rekognition')
-            response = rekognition.detect_labels(
-                Image={
-                    'S3Object': {
-                        'Bucket': bucket,
-                        'Name': imageName
-                    }
-                }
-            )
-
-            # Extrai os rótulos e confiança associada. Apenas os 4 primeiros rótulos serão mostrados
-            labels = []
-            for label in response['Labels'][:4]:  
-                labels.append({
-                    'Name': label['Name'],
-                    'Confidence': label['Confidence']
-                })
-
-            result = {
-                'url_to_image': url_to_image,
-                'created_image': get_image_creation_date(bucket, imageName),
-                'labels': labels
+    except botocore.exceptions.ClientError as e:
+        # Imagem não encontrada no bucket
+        error_message = "Imagem não encontrada no bucket."
+        response = create_http_response(500, {"error": error_message})
+        return response
+    
+    # Conecta-se ao AWS Rekognition e seleciona o serviço de reconhecimento de rótulos
+    rekognition = boto3.client('rekognition')
+    response = rekognition.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': imageName
             }
+        }
+    )
 
-            #Imprime o resultado nos logs do CloudWatch
-            print(result)
+    # Extrai os rótulos e confiança associada. Apenas os 4 primeiros rótulos serão mostrados
+    labels = []
+    for label in response['Labels'][:4]:  
+        labels.append({
+            'Name': label['Name'],
+            'Confidence': label['Confidence']
+        })
 
-            return create_http_response(200, result)
-
-    # Requisição inválida
-    error_message = {
-            'error': 'ImageNotFound',
-            'message': 'Imagem informada não existe.'
+    result = {
+        'url_to_image': url_to_image,
+        'created_image': get_image_creation_date(bucket, imageName),
+        'labels': labels
     }
-    #error_message = {'error': 'Requisição falhou! Imagem não localizada no bucket.'}
-    return create_http_response(500, error_message)
+
+    #Imprime o resultado nos logs do CloudWatch
+    print(result)
+
+    return create_http_response(200, result)
+
--- src/vision2.py
+++ src/vision2.py
@@ -1,15 +1,24 @@
 import boto3
+import botocore.exceptions
 import json
-from .utils import get_image_creation_date
+from .utils import get_image_creation_date, create_http_response
 
 def v2_vision(event, context):
-    
     request_body = json.loads(event['body'])
-    
     bucket = request_body['bucket']
     imageName = request_body['imageName']
-    
     url_to_image = f"https://{bucket}/{imageName}"
+    
+    # Verificar se a imagem existe no bucket
+    s3 = boto3.client('s3')
+    try:
+        s3.head_object(Bucket=bucket, Key=imageName)
+
+    except botocore.exceptions.ClientError as e:
+        # Imagem não encontrada no bucket
+        error_message = "Imagem não encontrada no bucket."
+        response = create_http_response(500, {"error": error_message})
+        return response
     
     # Conecta-se ao AWS Rekognition e seleciona o serviço de detecção de faces
     rekognition = boto3.client('rekognition')
@@ -57,43 +66,15 @@ def v2_vision(event, context):
             }
             faces.append(face_info)
 
-        #'created_image': '02-02-2023 17:00:00',
-
         result = {
             'url_to_image': url_to_image,
-            'created_image': get_image_creation_date(bucket, imageName),  
+            'created_image': get_image_creation_date(bucket, imageName),
             'faces': faces
         }
-    #Imprime o resultado nos logs do CloudWatch
+
+    # Imprime o resultado nos logs do CloudWatch
     print(result)
-    
-    # Retornar uma resposta HTTP válida
-    response = {
-        'statusCode': 200,
-        'body': json.dumps(result),
-        'headers': {
-            'Content-Type': 'application/json'
-        }
-    }
-        
+
+    response = create_http_response(200, result)
     return response
 
-'''
-def get_image_creation_date(bucket, imageName):
-    """
-    Obtém a data de criação de uma imagem no bucket do Amazon S3.
-
-    Args:
-        bucket (str): Nome do bucket do Amazon S3.
-        imageName (str): Nome da imagem.
-
-    Returns:
-        str: Data de criação da imagem no formato '%d-%m-%Y %H:%M:%S'.
-    """
-    s3 = boto3.client('s3')
-    
-    response = s3.head_object(Bucket=bucket, Key=imageName)
-    creation_date = response['LastModified'].strftime('%d-%m-%Y %H:%M:%S')
-    
-    return creation_date
-'''
